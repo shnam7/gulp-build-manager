5004b8ddb24e9c76d32a5cbda4f8e1728cf10f04
Robin Soo-hyuk Nam <shnam7@gmail.com>
2017-05-01 01:54:09 +0900

    'gbm init' warning message updated to inform -f option.

--- src/command.js
+++ src/command.js
@@ -6,7 +6,7 @@ import commander from 'commander';
 import upath from 'upath';
 
 commander
-  .version('1.2.12')
+  .version('1.2.13')
   .usage('[command] <options ...>');
 
 commander
@@ -18,15 +18,15 @@ commander
 
     if (!cmd.force) {
       if (fs.existsSync(upath.resolve(dir, 'gulpfile.babel.js'))) {
-        console.log('Warning:File already exists:gulpfile.babel.js. Process stopped.');
+        console.log('Warning:File already exists:gulpfile.babel.js. Use -f option to ignore it.');
         process.exit(0);
       }
       if (fs.existsSync(upath.resolve(dir, '.babelrc'))) {
-        console.log('Warning:File already exists:.babelrc. Process stopped.');
+        console.log('Warning:File already exists:.babelrc. Use -f option to ignore it.');
         process.exit(0);
       }
       if (fs.existsSync(upath.resolve(dir, 'gulp'))) {
-        console.log('Warning:Directory already exists:gulp. Process stopped.');
+        console.log('Warning:Directory already exists:gulp. Use -f option to ignore it.');
         process.exit(0);
       }
     }
