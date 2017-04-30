d83801ae41abe98ac763b9b15366f24e10eee9a3
Robin Soo-hyuk Nam <shnam7@gmail.com>
2017-05-01 01:46:45 +0900

    'gbm init' command-line tool now check for file overwriting, which can be bypassed using -f or --force option.

--- src/command.js
+++ src/command.js
@@ -1,27 +1,40 @@
 'use strict';
 
 import gulp from 'gulp';
-import commander from 'commander';
 import fs from 'fs';
+import commander from 'commander';
 import upath from 'upath';
-import is from './utils/is';
 
+commander
+  .version('1.2.12')
+  .usage('[command] <options ...>');
 
-function getVersion() {
-  let filepath = upath.resolve(__dirname, '../package.json');
-  return JSON.parse(fs.readFileSync(filepath)).version;
-}
+commander
+  .command('init [dir]')
+  .description('create project framework')
+  .option('-f, --force', 'force overwriting existing files')
+  .action((dir, cmd) => {
+    if (!dir) dir = '.';
 
-commander.usage('[command] <options ...>');
-commander.option('-v, --version', 'output the version number', () => {
-  let version = getVersion();
-  console.log('\ngbm v' + version + '\n');
-});
+    if (!cmd.force) {
+      if (fs.existsSync(upath.resolve(dir, 'gulpfile.babel.js'))) {
+        console.log('Warning:File already exists:gulpfile.babel.js. Process stopped.');
+        process.exit(0);
+      }
+      if (fs.existsSync(upath.resolve(dir, '.babelrc'))) {
+        console.log('Warning:File already exists:.babelrc. Process stopped.');
+        process.exit(0);
+      }
+      if (fs.existsSync(upath.resolve(dir, 'gulp'))) {
+        console.log('Warning:Directory already exists:gulp. Process stopped.');
+        process.exit(0);
+      }
+    }
 
-commander.command('init').description('create project framework').action((dir)=>{
-  if (!is.String(dir)) dir = '.';
-  gulp.src(upath.join(__dirname, '../templates/**/{*,.*}')).pipe(gulp.dest('.'));
-});
+    console.log(`Creating gulp-build-manager project in '${dir}' ...`);
+    gulp.src(upath.join(__dirname, '../templates/**/{*,.*}')).pipe(gulp.dest(dir));
+    console.log('--> Done.\n');
+  });
 
 commander.parse(process.argv);
 
