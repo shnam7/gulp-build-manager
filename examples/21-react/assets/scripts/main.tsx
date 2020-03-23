d2af6bf2e684485cf3b75fff60051ebc1a8733ad
Robin Soo-hyuk Nam <shnam7@gmail.com>
2020-03-24 02:38:50 +0900

    Add example #21-react

---
+++ examples/21-react/assets/scripts/main.tsx
@@ -0,0 +1,12 @@
+
+type MyProps = { msg: string };
+
+class MyComponent extends React.Component<MyProps> {
+    constructor(props: MyProps) { super(props); }
+
+    render() {
+        return <h1 className='title'>{this.props.msg}</h1>
+    }
+}
+
+ReactDOM.render(<MyComponent msg="Hello, React!"/>, document.getElementById('react-canvas'));
---
+++ examples/21-react/gbmconfig.js
@@ -0,0 +1,43 @@
+const gbm = require('../../lib');
+const upath = require('upath');
+
+const basePath = upath.relative(process.cwd(), __dirname);
+const srcRoot = upath.join(basePath, 'assets');
+const destRoot = upath.join(basePath, 'www');
+
+const projectName = upath.basename(__dirname);
+const prefix = projectName + ':';
+const port = 5000;
+
+const scss = {
+    buildName: 'scss',
+    builder: 'GCSSBuilder',
+    src: upath.join(srcRoot, 'scss/**/*.scss'),
+    dest: upath.join(destRoot, 'css'),
+    clean: upath.join(destRoot, 'css')
+}
+
+const scripts = {
+    buildName: 'scripts',
+    builder: 'GTypeScriptBuilder',
+    src: upath.join(srcRoot, 'scripts/**/*.ts*'),
+    dest: upath.join(destRoot, 'js'),
+    clean: upath.join(destRoot, 'js'),
+    addWatch: upath.join(basePath, 'tsconfig.json'),
+
+    buildOptions: {
+        tsConfig: upath.join(basePath, 'tsconfig.json')
+    }
+}
+
+module.exports = gbm.createProject({scss, scripts}, { prefix })
+    .addTrigger('@build', /.*/)
+    .addWatcher('@watch', {
+        watch: [upath.join(destRoot, '**/*.html')],  // watch files for reloader (no build actions)
+        browserSync: {
+            server: destRoot,
+            port: port + parseInt(prefix),
+            ui: { port: port + 100 + parseInt(prefix) }
+        },
+    })
+    .addCleaner();
